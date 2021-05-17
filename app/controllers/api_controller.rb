02208b4807576dbbb3bb15c44068f254acfaca9d
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-05-17 19:01:10 +0500

    add api helper controller

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,12 @@
+class ApiController < ApplicationController
+  skip_before_action :verify_authenticity_token
+ 
+  before_action :set_default_format
+  before_action :authenticate_user!
+
+  private
+
+  def set_default_format
+    request.format = :json
+  end
+end
