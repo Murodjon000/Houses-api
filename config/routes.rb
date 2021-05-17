2dedf1211c39f1f0f30353bf1bb8b6fe2e5d9165
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-05-17 19:01:45 +0500

    add routes to config

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  namespace :api do
+    namespace :v1 do
+      post :auth, to: "authentication#create"
+      resources :houses
+      resources :favourites
+      resources :users
+    end
+  end
+
 end
