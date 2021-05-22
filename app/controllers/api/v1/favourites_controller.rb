4f3d605f9716406840c70ea5667b734e0d7b2c03
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-05-22 16:20:59 +0500

    change favourites_controller methods

--- app/controllers/api/v1/favourites_controller.rb
+++ app/controllers/api/v1/favourites_controller.rb
@@ -1,36 +1,7 @@
 class Api::V1::FavouritesController < ApiController
   def index
-    favourites = current_user.favourites
-    if favourites
-      render json: favourites, status: 200
-    else
-      render json: { error: 'No favourites found!' }, status: 404
-    end
+    favourites = current_user.favourited_houses
+    render json: favourites, status: 200
   end
 
-  def create
-    favourite = current_user.favourites.new(set_params)
-
-    if favourite.save
-      render json: favourite, status: 201
-    else
-      render json: { error: 'House was not added to the favourites.' }, status: 404
-    end
-  end
-
-  def destroy
-    favourite = current_user.favourites.find(params[:id])
-    if favourite
-      favourite.destroy
-      render json: { message: 'Successfuly removed', removed_item: favourite }, status: 200
-    else
-      render json: { error: 'Sorry house could not be removed' }, status: 422
-    end
-  end
-
-  private
-
-  def set_params
-    params.permit(:house_id)
-  end
 end
