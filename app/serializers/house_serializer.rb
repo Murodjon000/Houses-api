1219f319ecd8083a104a6d4a5e7f43a201f24b1d
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-05-22 16:22:29 +0500

    update house serialiezer

--- app/serializers/house_serializer.rb
+++ app/serializers/house_serializer.rb
@@ -2,4 +2,5 @@ class HouseSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :price, :image, :built_date, :number_of_rooms, :location
 
   has_many :favourites
+  has_many :favouritors, through: :favourites, source: :user
 end
