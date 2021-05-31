0a9d02759776256a8046b0f0ae98a9d81bc0698b
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-05-31 21:53:54 +0500

    add avatar to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,25 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :favourites
   has_many :favourited_houses, through: :favourites, source: :house
 
-  validates :username, uniqueness: true, presence: true, length: {minimum: 3, maximum: 15}
+  validates :username, uniqueness: true, presence: true, length: { minimum: 3, maximum: 15 }
+  # validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Must be valid email address" }
+
+  has_one_attached :avatar
+
+  def self.avatar_url(avatar)
+    if avatar.attached?
+      avatar.blob.service_url
+    end
+  end
+
+
+
+
 end
