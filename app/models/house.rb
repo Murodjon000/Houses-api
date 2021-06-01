6d2bcdf7af8d97872babaedcd645dd2426b6e0e3
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-06-01 22:49:56 +0500

    update house model

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,5 +1,13 @@
 class House < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
   has_many :favourites
   has_many :favouritors, through: :favourites, source: :user
-  validates_presence_of :name, :description, :price, :image, :built_date, :number_of_rooms, :location
+  validates_presence_of :name, :description, :price,  :built_date, :number_of_rooms, :location
+
+  def image_url
+    if image.attached?
+      image.blob.service_url
+    end
+  end
 end
