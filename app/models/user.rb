fe0d55c8d092b15d41da050bcaeb69af10c73cbc
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-06-03 23:15:38 +0500

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,17 +9,14 @@ class User < ApplicationRecord
   has_many :favourited_houses, through: :favourites, source: :house
 
   validates :username, uniqueness: true, presence: true, length: { minimum: 3, maximum: 15 }
-  # validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Must be valid email address" }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Must be valid email address" }
 
   has_one_attached :avatar
   has_many :houses
+
   def self.avatar_url(avatar)
     if avatar.attached?
-      avatar.blob.service_url
+      avatar.blob.url
     end
   end
-
-
-
-
 end
