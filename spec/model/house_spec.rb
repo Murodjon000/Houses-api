28a88520a6b77353730f2ff023959f55a038d046
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-06-03 23:15:05 +0500

    add house model spec

--- spec/model/house_spec.rb
+++ spec/model/house_spec.rb
@@ -3,15 +3,19 @@ require 'rails_helper'
 RSpec.describe House, type: :model do
   describe 'associations' do
     it { should have_many(:favourites) }
+    it { should have_many(:favouritors) }
+    it { should have_one_attached(:images) }
+    it { should belong_to(:user) }
   end
 
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:price) }
-    it { should validate_presence_of(:image) }
+    it { should validate_numericality_of(:price) }
     it { should validate_presence_of(:built_date) }
     it { should validate_presence_of(:number_of_rooms) }
+    it { should validate_numericality_of(:number_of_rooms) }
     it { should validate_presence_of(:location) }
   end
 end
