9847f17c24a2ac0e59fe2f3071234c15b5ad4969
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-06-03 23:15:20 +0500

    add house model

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,13 +1,16 @@
 class House < ApplicationRecord
   belongs_to :user
-  has_one_attached :image
+  has_one_attached :images
   has_many :favourites
   has_many :favouritors, through: :favourites, source: :user
-  validates_presence_of :name, :description, :price,  :built_date, :number_of_rooms, :location
+  validates_presence_of :name, :description, :price, :built_date, :number_of_rooms, :location
+  validates :price, numericality: { greater_than: 0 }
+  validates :number_of_rooms, numericality: { greater_than: 1 }
 
-  def image_url
-    if image.attached?
-      image.blob.service_url
+  def image_url(images)
+    if images.attached?
+      images.blob.url
     end
   end
+
 end
