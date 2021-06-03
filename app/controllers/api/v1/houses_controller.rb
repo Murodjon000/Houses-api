90e0b7730911bd6544c256951ba6d98066c4143f
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-06-03 23:16:00 +0500

    update houses controller with aws upload

--- app/controllers/api/v1/houses_controller.rb
+++ app/controllers/api/v1/houses_controller.rb
@@ -1,7 +1,29 @@
 class Api::V1::HousesController < ApiController
+  skip_before_action :authenticate_user!
   def index
     houses = House.all
     render json: houses, status: 200
+  end
+
+  def create
+    image = params[:house][:house_image]
+    params = house_params.except(:house_image)
+    house = current_user.houses.create(params)
+
+    if image.present?
+      house.images.attach(image)
+    else
+      @error = ["Image can`t be blank"]
+    end
+    
+    house_url = house.image_url(house.images)
+    house.image = house_url
+
+    if house.save
+      render json: { house: house }, status: 200
+    else
+      render json: {message: house.errors.full_messages }, status: 400
+    end
   end
 
   def show
@@ -13,12 +35,15 @@ class Api::V1::HousesController < ApiController
     end
   end
 
+
   def favourite
     if current_user.favourites.exists?(house_id: params[:house_id])
       render json: { message: 'You\'ve already favourited this house!' }, status: :forbidden
     else
-      current_user.favourited_houses << House.find(params[:house_id])
-      render json: { message: 'House successfuly added to the favourites.' }
+      house = House.find(params[:house_id])
+      house.image =  house.image_url(house.images)
+      current_user.favourited_houses << house
+      render json: { message: "House successfuly added to your favourites" }
     end
   end
 
@@ -26,5 +51,11 @@ class Api::V1::HousesController < ApiController
     favourite = current_user.favourites.find_by(house_id: params[:house_id])
     favourite&.delete
     render json: { message: 'Successfuly removed' }
+  end
+
+  private
+  
+  def house_params
+    params.require(:house).permit(:name, :description, :price, :built_date, :number_of_rooms, :location, :image, :house_image)
   end
 end
