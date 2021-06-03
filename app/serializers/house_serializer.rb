c0466528761166ca2c8d7b09d912782aef03437b
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-06-03 23:16:47 +0500

    update houses serializer

--- app/serializers/house_serializer.rb
+++ app/serializers/house_serializer.rb
@@ -1,6 +1,14 @@
 class HouseSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :price, :image, :built_date, :number_of_rooms, :location
-
+  attributes :id, :name, :description, :price, :built_date, :images,  :number_of_rooms, :location
+  belongs_to :user
   has_many :favourites
   has_many :favouritors, through: :favourites, source: :user
+ 
+
+  def images
+    if object.images.attached?
+      object.images.blob.url
+    end
+  end
+
 end
