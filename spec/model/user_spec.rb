cb408318c9a6426853cf00cd88564b6611dfb997
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-06-03 23:14:57 +0500

    add user model spec

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -3,9 +3,13 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'associations' do
     it { should have_many(:favourites) }
+    it { should have_many(:houses) }
+    it { should have_one_attached(:avatar) }
+    it { should have_many(:favourited_houses) }
   end
 
   describe 'validations' do
     it { should validate_presence_of(:username) }
+    it { should validate_length_of(:username).is_at_most(15)  }
   end
 end
