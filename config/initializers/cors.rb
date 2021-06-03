c5c43a1602002de365c97e2ab40b3021cc069d4d
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-06-03 23:17:44 +0500

    update config folder

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
    # for development
    allow do
-    origins 'http://localhost:3000'
+    origins 'http://localhost:3001'
   
     resource '*',
       headers: :any,
